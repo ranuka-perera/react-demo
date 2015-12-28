3de30e80bad1b673e2fafac0760fa2aa31b50c3b
Ranuka Perera <ranukap@gmail.com>
2015-12-28 05:59:40 +0530

    * Add thumbs to page.

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,26 @@
 var ThumbImage = React.createClass({
     render: function () {
         return (
-            <div></div>
+            <li className="thumb">
+                <img src={this.props.url} alt={this.props.altText} className="thumb-image"/>
+            </li>
         );
     }
 });
 
 var ThumbList = React.createClass({
     render: function () {
+        var count = 0;
+        var thumbs = this.props.images.map(function (image) {
+            count += 1;
+            return (
+                <ThumbImage url={image.url} altText={image.title} key={count}/>
+            );
+        });
         return (
-            <div></div>
+            <ul className="thumbList">
+                {thumbs}
+            </ul>
         );
     }
 });
@@ -28,7 +39,7 @@ var MainPage = React.createClass({
         var images = [];
         jsonData.forEach(function (item) {
             if (item.mime_type === 'image/jpeg' || item.mime_type === 'image/png') {
-                images.push(item.path);
+                images.push({url: item.path, title: item.title});
             }
         });
         return images;
