f4f4c8e373255280a35e417e25b3f30115e2472d
Ranuka Perera <random@sawrc.com>
2015-12-28 09:05:44 +0530

    Update 2015-12-28T03:35:15.588Z

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-    upload_gh = require('gulp-gh-pages');
-
-// Deploy to github gh-pages branch.
-gulp.task('deploy', function () {
-    return gulp.src(['index.html', 'css/**/*', 'scripts/**/*', 'assets/**/*'], {base: './'})
-        .pipe(upload_gh());
-});
