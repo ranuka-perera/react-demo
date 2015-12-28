7a0d8eaf93782087eec2ec05b536d9e7900c5a10
Ranuka Perera <ranukap@gmail.com>
2015-12-28 05:32:50 +0530

    * Complete ajax loading.

--- scripts/app.js
+++ scripts/app.js
@@ -24,31 +24,38 @@ var Cropper = React.createClass({
 
 var MainPage = React.createClass({
     // Return the image array.
-    parseData: function (json_data) {
-
+    parseData: function (jsonData) {
+        var images = [];
+        jsonData.forEach(function (item) {
+            if (item.mime_type === 'image/jpeg' || item.mime_type === 'image/png') {
+                images.push(item.path);
+            }
+        });
+        return images;
     },
     getInitialState: function () {
         return {images: []};
     },
     componentDidMount: function () {
         // When Component loads
-        $.ajax(
-            {
-                url: this.props.url,
-                crossDomain: true,
-                dataType: 'jsonp',
-                jsonp: 'jsoncallback',
-                type: 'GET',
-                success: function (data) {
-                    this.setState({images: this.parseData(data)});
-                }.bind(this)
-            }
-        );
+        $.ajax({
+            url: this.props.url,
+            crossDomain: true,
+            dataType: 'jsonp',
+            jsonp: 'jsoncallback',
+            type: 'GET',
+            success: function (data) {
+                this.setState({images: this.parseData(data)});
+            }.bind(this),
+            error: function () {
+                alert("Data failed to load");
+            }.bind(this)
+        });
     },
     render: function () {
         return (
             <div className="main-page">
-                <ThumbList />
+                <ThumbList images={this.state.images}/>
                 <Cropper />
             </div>
         );
@@ -56,6 +63,6 @@ var MainPage = React.createClass({
 });
 
 ReactDOM.render(
-    <MainPage url="https://www.yamu.lk/api/?v=2.0&start=0&size=20&section=public&action=getAllAttachments" />,
+    <MainPage url="//www.yamu.lk/api/?v=2.0&start=0&size=20&section=public&action=getAllAttachments" />,
     document.getElementById('content')
 );
