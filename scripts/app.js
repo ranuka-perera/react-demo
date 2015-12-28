b5753258c3750ff1c4a377d20ae19e4db5768893
Ranuka Perera <ranukap@gmail.com>
2015-12-28 07:12:02 +0530

    * Add thumbnail login, but cloudfront don't work with CORS.

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,35 @@
 var ThumbImage = React.createClass({
+    generateThumbnail: function (url, dimensions, targetWidth, callback) {
+        // Based on https://stackoverflow.com/a/7557690 & https://stackoverflow.com/a/6005211
+        var canvas = document.createElement("canvas");
+        var img = new Image();
+        img.crossOrigin = 'anonymous';
+        var tempScale = targetWidth / dimensions.width;
+
+        canvas.width = dimensions.width * tempScale;
+        canvas.height = dimensions.height * tempScale;
+        var ctx = canvas.getContext("2d");
+        img.onload = function () {
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+            var thumbUrl = canvas.toDataURL("image/png");
+            if (callback) {
+                callback(thumbUrl);
+            }
+        };
+        img.src = url;
+    },
+    getInitialState: function () {
+        return {url: ''};
+    },
+    componentDidMount: function () {
+        this.generateThumbnail(this.props.url, this.props.dimensions, 300, function (thumbUrl) {
+            this.setState({url: thumbUrl});
+        });
+    },
     render: function () {
         return (
             <li className="thumb">
-                <img src={this.props.url} alt={this.props.altText} className="thumb-image"/>
+                <img src={this.state.url} alt={this.props.altText} className="thumb-image"/>
             </li>
         );
     }
@@ -14,7 +41,7 @@ var ThumbList = React.createClass({
         var thumbs = this.props.images.map(function (image) {
             count += 1;
             return (
-                <ThumbImage url={image.url} altText={image.title} key={count}/>
+                <ThumbImage url={image.url} altText={image.title} dimensions={image.dimensions} key={count}/>
             );
         });
         return (
@@ -22,6 +49,11 @@ var ThumbList = React.createClass({
                 {thumbs}
             </ul>
         );
+    }
+});
+var CropperImage = React.createClass({
+    render: function () {
+        return <div></div>;
     }
 });
 
@@ -39,7 +71,7 @@ var MainPage = React.createClass({
         var images = [];
         jsonData.forEach(function (item) {
             if (item.mime_type === 'image/jpeg' || item.mime_type === 'image/png') {
-                images.push({url: item.path, title: item.title});
+                images.push({url: item.path, title: item.title, dimensions: {width: item.width, height: item.height}});
             }
         });
         return images;
